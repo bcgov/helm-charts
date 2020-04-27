09c1a0591dfd04722ae1a3774586facc9c4958f6
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-04-27 11:24:21 -0700

    Adding repository functionality, forum api and storage-api

---
+++ package.sh
@@ -0,0 +1,35 @@
+echo "helm chart releaser must be installed prior to running this (https://github.com/helm/chart-releaser)"
+read -p "You must set CR_TOKEN in env before running this, proceed? [yN]" yn
+case $yn in
+    [Yy]* ) echo "Proceeding";;
+    * ) echo "Aborting"; exit;;
+esac
+
+rm -rf packages/*
+
+cd forum-api
+helm dep up
+cd ..
+helm package forum-api
+
+cd forum-api
+helm dep up
+cd ..
+helm package nifi
+
+cd forum-api
+helm dep up
+cd ..
+helm package storage-api
+
+cd forum-api
+helm dep up
+cd ..
+helm package vdi-virtual-display
+
+
+mv *.tgz packages/.
+
+cr upload --owner bcgov --git-repo helm-charts --package-path ./packages
+
+cr index --owner bcgov --git-repo helm-charts --package-path ./packages --charts-repo https://github.com/bcgov/helm-charts/releases
